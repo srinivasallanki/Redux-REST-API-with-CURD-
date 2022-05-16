4ca27f2bfbf6b40d3dd932461bd40429644cb70b
srinivasallanki <srinivas5859@gmail.com>
2022-05-16 07:13:11 +0530

    one folder detelted

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import usersReducers from './users/usersReducers'
-
-const rootReducer = combineReducers({
-  users: usersReducers,
-})
-
-export default rootReducer
